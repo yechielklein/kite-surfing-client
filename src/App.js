0f24cea1930a8445b6227f376e84aa2abd8e7138
Yechiel Klein <kleinyechiel@gmail.com>
2022-10-20 10:12:05 -0400

    get message from server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [message, updateMessage] = useState("")
+  useEffect(() => {
+	axios.post("http://localhost:5000/hi", {hifrom: "usher the chair"}).then(x => updateMessage(x.data))
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
