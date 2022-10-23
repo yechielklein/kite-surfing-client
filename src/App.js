1e2b8f5ddf4dd024b4ed39859abb9c201e77ae33
Yechiel Klein <kleinyechiel@gmail.com>
2022-10-22 21:46:01 -0400

    prettify

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [message, updateMessage] = useState("")
-  useEffect(() => {
-	  axios.post(`${process.env.REACT_APP_API_URL}/hi`, {hifrom: "usher the chair"}).then(x => updateMessage(x.data))
-  })
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {message}
-        </p>
-      </header>
-    </div>
-  );
+	const [message, updateMessage] = useState("")
+	useEffect(() => {
+		axios.post(
+			`${process.env.REACT_APP_API_URL}/hi`,
+			{hifrom: "usher the chair"}
+		).then(x => updateMessage(x.data))
+	})
+	return (
+		<div className="App">
+    	<header className="App-header">
+    		<p>
+        		{message}
+        	</p>
+    	</header>
+    	</div>
+	);
 }
 
 export default App;
