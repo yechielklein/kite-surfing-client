2b1d3e83480f7450c3375082bde92a359670bdd6
Yechiel Klein <kleinyechiel@gmail.com>
2022-10-24 18:49:14 -0400

    make Home and Kites pages

--- src/App.js
+++ src/App.js
@@ -1,36 +1,60 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-	const [kites, updateKites] = useState([])
-	useEffect(() => {
-		axios.get(
-			`${process.env.REACT_APP_API_URL}/kites`,
-			{name: 'test', email: 'test@gmail.com'}
-		).then(x => updateKites(x.data))
-	}, [])
+const App = () => (
+	<div className="App">
+		<h1>Welcome to kite surfing client</h1>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="kites" element={<Kites />} />
+		</Routes>
+	</div>
+);
+
+const Home = () =>(
+	<div>
+		<main>
+			<h2>Welcome to the homepage!</h2>
+		</main>
+		<nav>
+			<Link to="/kites">Kites</Link>
+		</nav>
+	</div>
+);
+
+const Kites = () => {
+    const [kites, updateKites] = useState([]);
+    useEffect(() => {
+        axios
+            .get(`${process.env.REACT_APP_API_URL}/kites`)
+            .then((x) => updateKites(x.data));
+    }, []);
 	return (
-		<div className="App">
-    	<header className="App-header">
-    		<p>
-        		{ kites.map(k => <Kite {...k} />)}
-        	</p>
-    	</header>
-    	</div>
-	);
+        <div className="App-header">
+            <p>
+                {kites.map((k) => (
+                    <Kite {...k} />
+                ))}
+            </p>
+			<nav>
+				<Link to="/">Home</Link>
+			</nav>
+        </div>
+    );
 }
 
-export default App;
-
 const Kite = (props) => (
-	<p>
-		Brand: {props.brand} &nbsp;
+    <p>
+        Brand: {props.brand} &nbsp;
 		Model: {props.model} &nbsp;
-		Year: {props.year} &nbsp;
+		Year:{` ${props.year}`} &nbsp;
 		Size: {props.size} meters squared &nbsp;
 		Price: ${props.price / 100} &nbsp;
-		Location: {props.address}, {props.city}, {props.country} &nbsp;&nbsp;
-		{!!props.notes ? `Notes: ${props.notes} ` : 'No notes'}
-	</p>
-)
+		Location: {`${props.address}, ${props.city}, ${props.country}`} &nbsp;&nbsp;
+        {props.notes ? `Notes: ${props.notes} ` : 'No notes'}
+    </p>
+);
+
+export default App;
