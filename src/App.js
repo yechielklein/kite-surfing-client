facf19f9ecc97143ea6128080330474dd6b53c0f
Yechiel Klein <kleinyechiel@gmail.com>
2022-10-23 23:53:33 -0400

    define kite element

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-	const [message, updateMessage] = useState("")
+	const [kites, updateKites] = useState([])
 	useEffect(() => {
-		axios.post(
-			`${process.env.REACT_APP_API_URL}/hi`,
-			{hifrom: "usher the chair"}
-		).then(x => updateMessage(x.data))
+		axios.get(
+			`${process.env.REACT_APP_API_URL}/kites`
+		).then(x => updateKites(x.data))
 	})
 	return (
 		<div className="App">
     	<header className="App-header">
     		<p>
-        		{message}
+        		{ kites.map(k => <Kite {...k} />)}
         	</p>
     	</header>
     	</div>
@@ -22,3 +21,15 @@ function App() {
 }
 
 export default App;
+
+const Kite = (props) => (
+	<p>
+		Brand: {props.brand} &nbsp;
+		Model: {props.model} &nbsp;
+		Year: {props.year} &nbsp;
+		Size: {props.size} meters squared &nbsp;
+		Price: ${props.price / 100} &nbsp;
+		Location: {props.address}, {props.city}, {props.country} &nbsp;&nbsp;
+		{!!props.notes ? `Notes: ${props.notes} ` : 'No notes'}
+	</p>
+)
