1b0f238ae32228921dc53f40f2fd33d04e3bf594
Yechiel Klein <kleinyechiel@gmail.com>
2022-10-24 18:58:27 -0400

    prettify

--- src/App.js
+++ src/App.js
@@ -45,14 +45,14 @@ const Kites = () => {
     );
 }
 
-
-
 const Kite = (props) => (
     <p>
-        Brand: {props.brand} &nbsp; Model: {props.model} &nbsp; Year:{' '}
-        {props.year} &nbsp; Size: {props.size} meters squared &nbsp; Price: $
-        {props.price / 100} &nbsp; Location: {props.address}, {props.city},{' '}
-        {props.country} &nbsp;&nbsp;
+        Brand: {props.brand} &nbsp;
+		Model: {props.model} &nbsp;
+		Year:{` ${props.year}`} &nbsp;
+		Size: {props.size} meters squared &nbsp;
+		Price: ${props.price / 100} &nbsp;
+		Location: {`${props.address}, ${props.city}, ${props.country}`} &nbsp;&nbsp;
         {props.notes ? `Notes: ${props.notes} ` : 'No notes'}
     </p>
 );
