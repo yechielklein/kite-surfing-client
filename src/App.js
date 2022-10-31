df5cd922adf72f7b831b952f4219a61676eee78b
Yechiel Klein <kleinyechiel@gmail.com>
2022-10-31 13:51:16 -0400

    move links to app.js import and add route for addKite

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import Home from "./components/home";
 import Kites from "./components/kites";
 import SignUp from "./components/signUp";
+import AddKite from "./components/addKite";
 
 const App = () => (
     <div className="App">
@@ -12,7 +13,14 @@ const App = () => (
             <Route path="/" element={<Home />} />
             <Route path="kites" element={<Kites />} />
             <Route path="signup" element={<SignUp />} />
+            <Route path="addkite" element={<AddKite />} />
         </Routes>
+		<nav>
+		    <Link to="/">Home</Link> &nbsp;
+			<Link to="/kites">Available Kites</Link> &nbsp;
+			<Link to="/signup">Sign Up</Link> &nbsp;
+			<Link to="/addkite">Add a Kite</Link>
+		</nav>
     </div>
 );
 
--- src/components/kite.js
+++ src/components/kite.js
@@ -5,7 +5,7 @@ const Kite = (props) => (
 		Year: {props.year} &nbsp;
 		Size: {props.size} meters squared &nbsp;
         Price: ${props.price / 100} &nbsp;
-		Location:{`${props.address}, ${props.city}, ${props.country}`} &nbsp; &nbsp;
+		Location: {`${props.address}, ${props.city}, ${props.country}`} &nbsp; &nbsp;
         {props.notes ? `Notes: ${props.notes} ` : 'No notes'}
     </p>
 );
