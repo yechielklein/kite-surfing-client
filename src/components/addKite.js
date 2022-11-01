c799bed068d19e67708c2bb8decf71e97f800762
Yechiel Klein <kleinyechiel@gmail.com>
2022-11-01 15:27:03 -0400

    use useState

--- src/components/addKite.js
+++ src/components/addKite.js
@@ -1,59 +1,116 @@
 import axios from 'axios';
+import { useState } from 'react';
 
-const handleSubmit = async () => (
-	(await axios.post(
-		`${process.env.REACT_APP_API_URL}/addKite`,
-		{
-			sellerId: Number(document.getElementById('sellerId').value),
-			price: Math.floor(Number(document.getElementById('price').value) * 100),
-			brand: document.getElementById('brand').value,
-			model: document.getElementById('model').value,
-			year: Number(document.getElementById('year').value),
-			size: Number(document.getElementById('size').value),
-			country: document.getElementById('country').value,
-			city: document.getElementById('city').value,
-			address: document.getElementById('address').value,
-			notes: document.getElementById('notes').value
-		}
-	)).data
-);
+const AddKite = () => {
+    const [sellerId, updateSellerId] = useState('');
+    const [price, updatePrice] = useState('');
+    const [brand, updateBrand] = useState('');
+    const [model, updateModel] = useState('');
+    const [year, updateYear] = useState('');
+    const [size, updateSize] = useState('');
+    const [country, updateCountry] = useState('');
+    const [city, updateCity] = useState('');
+    const [address, updateAddress] = useState('');
+    const [notes, updateNotes] = useState('');
 
-const AddKite = () => (
-    <div>
-        <form onSubmit={handleSubmit}>
-            User ID*: &nbsp;
-            <input id="sellerId" required />
-            <br /> <br />
-            Price*: &nbsp;
-            <input id="price" required />
-            <br /> <br />
-            Brand*: &nbsp;
-            <input id="brand" required />
-            <br /> <br />
-            Model*: &nbsp;
-            <input id="model" required />
-            <br /> <br />
-            Year*: &nbsp;
-            <input id="year" required />
-            <br /> <br />
-            Size*: &nbsp;
-            <input id="size" required />
-            <br /> <br />
-            Country*: &nbsp;
-            <input id="country" required />
-            <br /> <br />
-            City*: &nbsp;
-            <input id="city" required />
-            <br /> <br />
-            Address*: &nbsp;
-            <input id="address" required />
-            <br /> <br />
-            Notes: &nbsp;
-            <input id="notes" />
-            <br /> <br />
-            <input type="submit" value="Add Kite" />
-        </form>
-    </div>
-);
+    const handleSubmit = async () => (
+        (await axios.post(
+			`${process.env.REACT_APP_API_URL}/addKite`,
+			{
+				sellerId: Number(sellerId),
+				price: Math.floor(Number(price) * 100),
+				brand,
+				model,
+				year: Number(year),
+				size: Number(size),
+				country,
+				city,
+				address,
+				notes
+			}
+		)).data
+	);
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                User ID*: &nbsp;
+                <input
+                    value={sellerId}
+                    onChange={(e) => updateSellerId(e.target.value)}
+                    pattern="\d+"
+                    required
+                />
+                <br /> <br />
+                Price*: &nbsp;
+                <input
+                    value={price}
+                    onChange={(e) => updatePrice(e.target.value)}
+                    pattern="\d+"
+                    required
+                />
+                <br /> <br />
+                Brand*: &nbsp;
+                <input
+                    value={brand}
+                    onChange={(e) => updateBrand(e.target.value)}
+                    required
+                />
+                <br /> <br />
+                Model*: &nbsp;
+                <input
+                    value={model}
+                    onChange={(e) => updateModel(e.target.value)}
+                    required
+                />
+                <br /> <br />
+                Year*: &nbsp;
+                <input
+                    value={year}
+                    onChange={(e) => updateYear(e.target.value)}
+                    pattern="\d+"
+                    required
+                />
+                <br /> <br />
+                Size*: &nbsp;
+                <input
+                    value={size}
+                    onChange={(e) => updateSize(e.target.value)}
+                    pattern="\d+"
+                    required
+                />
+                <br /> <br />
+                Country*: &nbsp;
+                <input
+                    value={country}
+                    onChange={(e) => updateCountry(e.target.value)}
+                    required
+                />
+                <br /> <br />
+                City*: &nbsp;
+                <input
+                    value={city}
+                    onChange={(e) => updateCity(e.target.value)}
+                    required
+                />
+                <br /> <br />
+                Address*: &nbsp;
+                <input
+                    value={address}
+                    onChange={(e) => updateAddress(e.target.value)}
+                    required
+                />
+                <br /> <br />
+                Notes: &nbsp;
+                <input
+                    value={notes}
+                    onChange={(e) => updateNotes(e.target.value)}
+                />
+                <br /> <br />
+                <input type="submit" value="Add Kite" />
+            </form>
+        </div>
+    );
+};
 
 export default AddKite;
--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,47 +1,60 @@
-import axios from "axios";
+import axios from 'axios';
+import { useState } from 'react';
 
-const handleSubmit = async () => (
-	(await axios.post(
-		`${process.env.REACT_APP_API_URL}/signup`,
-		{
-			name: document.getElementById('name').value,
-			email: document.getElementById('email').value,
-			password: document.getElementById('password').value
-		}
-	)).data
-);
+const SignUp = () => {
+    const [name, updateName] = useState('');
+    const [email, updateEmail] = useState('');
+    const [password, updatePassword] = useState('');
 
-const SignUp = () => (
-    <div>
-        <form onSubmit={handleSubmit}>
-            Name: &nbsp;
-            <input id="name" placeholder="John Doe" />
-            <br />
-            <br />
-            Email*: &nbsp;
-            <input
-                type="email"
-                id="email"
-                placeholder="johndoe@example.com"
-                required
-            />
-            <br />
-            <br />
-            Password*:&nbsp;
-            <input
-                type="password"
-                id="password"
-                required
-                placeholder="Password123"
-                pattern="(?=^.{8,}$)(?=.*\d)(?=.*[A-Z])(?=.*[a-z]).*$"
-            />
-            <br />
-            (Must have at least 8 characters, a capital letter, a lowercase letter and a number)
-            <br />
-            <br />
-            <input type="submit" value="Sign up" />
-        </form>
-    </div>
-);
+    const handleSubmit = async () => (
+        (await axios.post(
+			`${process.env.REACT_APP_API_URL}/signup`,
+			{
+				name,
+				email,
+				password
+			}
+		)).data
+	);
 
-export default SignUp
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                Name: &nbsp;
+                <input
+                    placeholder="John Doe"
+                    value={name}
+                    onChange={(e) => updateName(e.target.value)}
+                />
+                <br />
+                <br />
+                Email*: &nbsp;
+                <input
+                    type="email"
+                    placeholder="johndoe@example.com"
+                    value={email}
+                    onChange={(e) => updateEmail(e.target.value)}
+                    required
+                />
+                <br />
+                <br />
+                Password*:&nbsp;
+                <input
+                    type="password"
+                    placeholder="Password123"
+                    value={password}
+                    onChange={(e) => updatePassword(e.target.value)}
+                    required
+                    pattern="(?=^.{8,}$)(?=.*\d)(?=.*[A-Z])(?=.*[a-z]).*$"
+                />
+                <br />
+                (Must have at least 8 characters, a capital letter, a lowercase letter and a number)
+                <br />
+                <br />
+                <input type="submit" value="Sign up" />
+            </form>
+        </div>
+    );
+};
+
+export default SignUp;
